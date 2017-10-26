5f5dbdd8b4611cf208c889ca7d1c6b295bc8efae
Jacek Wielemborek <d33tah@gmail.com>
2017-10-26 18:26:49 +0200

    kompilujemy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::boxed::Box;
+use std::collections::HashMap;
 
 enum BencodedObject {
     Int(i32),
     ByteString(String),
     List(Vec<Box<BencodedObject>>),
-    Dict
+    Dict(std::map
 }
 
 fn main() {
