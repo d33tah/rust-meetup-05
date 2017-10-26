1af32eeb40e921a3f191c774ec79fb57594f63e7
Jacek Wielemborek <d33tah@gmail.com>
2017-10-26 19:23:19 +0200

    kompilujemy

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ enum BencodedObject {
 impl Hash for BencodedObject {
     fn hash<H: Hasher>(&self, state: &mut H) {
         match self {
-            BencodedObject::Int(x) => x.hash(state),
+            BencodedObject::Int(*x) => x.hash(state),
             BencodedObject::ByteString(*x) => x.hash(state),
             _ => {}
         }
