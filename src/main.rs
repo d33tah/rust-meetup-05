3cdc36c63903bdc74a512d1d7e8ff5f426b128cd
Jacek Wielemborek <d33tah@gmail.com>
2017-10-26 18:38:19 +0200

    kompilujemy

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ enum BencodedObject {
 fn decode(encoded: String) -> Result<BencodedObject,String> {
     let mut bencoded_type: char;
     for (i, n) in encoded.chars().enumerate() {
-
+        if i > 0 && !bencoded_type {
+            return Err("Type not set");
+        }
     }
     Ok(BencodedObject::Int(1))
 }
